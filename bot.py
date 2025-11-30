0bc176eb2824c02a0b2c66f4fee7f4746b496270
krimo9004-stack <krimo9004@gmail.com>
2025-11-30 21:07:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,26 @@
 import telebot
 
-bot = telebot.TeleBot("8404730679:AAHSlx4Kwhxc7rhgTc9pXxAyStldlQclBN8")
+TOKEN = "8404730679:AAHSlx4Kwhxc7rhgTc9pXxAyStldlQclBN8"
+
+bot = telebot.TeleBot(TOKEN)
+
+# رابط الإعلان الخاص بك
+ad_link = "https://ouo.io/HPS3uw"
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "  https://ouo.io/YOUR_SHORT_LINK:\n\nhttps://ouo.io/YOUR_SHORT_LINK")
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    btn = telebot.types.InlineKeyboardButton(
+        text="🔗 اضغط لمشاهدة الإعلان",
+        url=ad_link
+    )
+    keyboard.add(btn)
 
+    bot.send_message(
+        message.chat.id,
+        "👋 أهلاً بك!\n\nاضغط على الزر لمشاهدة الإعلان وكسب النقاط 🔥",
+        reply_markup=keyboard
+    )
+
+print("Bot is running...")
 bot.polling()
